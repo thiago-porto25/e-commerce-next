1c3317e55c1d7f5e57edf28831a25674056116a5
Thiago Porto <thiago.porto@d3.do>
2022-05-03 16:39:37 -0300

    add glbal css and Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next e-commerce</title>
+        <meta
+          name="description"
+          content="Fake e-commerce site made with NextJS, Sanity CMS and Stripe"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
